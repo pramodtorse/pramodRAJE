b454d157addacdead0ce9731e1d4f7b2ebbff4f3
pramod <pramodtorse@gmail.com>
2022-01-02 12:39:44 +0530

    Frist Commit

---
+++ `PravinTorase/src/selenium/CoreBrushUp.java
@@ -0,0 +1,24 @@
+package selenium;
+
+public class CoreBrushUp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello PraMod");
+		
+		
+		
+		
+		
+		
+		
+		
+		 
+		
+		
+	
+		
+		
+		
+	}
+
+}
